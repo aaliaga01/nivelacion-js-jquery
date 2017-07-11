228a5d671d24069d50b7acdd3736a067165932f5
aaliaga-casa <a.aliaga.val@gmail.com>
2017-07-10 23:59:16 -0300

    etapa 8, se agrega y quita la clase make, pero no hacen nada, no entendi bien que tenia que pasar y sn las 23:59, hasta aqui no mas llegamos

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -3,5 +3,25 @@ $(document).ready( function(){
 
 	$(".js-back").show();
 	$(".js-menu").hide();
-	
+
+	//llamado funcion agregaclase
+	agregaClase();
+
+	//llamado funcion eliminaclase
+	eliminaClase();
+
 });
+
+function agregaClase(){
+	$(".js-show-make").click(function(){;
+		console.log("Supongo que aqui se deberían publicar las recetas ")
+		$(".btns-group").addClass(".make");
+		
+	});
+}
+
+function eliminaClase(){
+	$(".js-show-recipe").click(function(){
+		$(".btns-group").removeClass(".make");
+	});
+}
